fca4e819ed5e4f5cd03ca19b00348847eef7e9b8
Rohan30020407 <rohansoniblphp@gmail.com>
2026-09-09 15:05:43 +0530

    Updated Expense-Tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-import React,{useState} from 'react'
+import React,{useState , useEffect,useRef} from 'react'
 import Add from './components/AddExpense'
+import './App.css'
 
 const App = () => {
-  const [expense,setExpense] = useState([])
+    const localData = JSON.parse(localStorage.getItem("expenses"))
+  const [expense,setExpense] = useState(localData !== null ? localData:[])
+  useEffect(()=>{
+    localStorage.setItem("expenses",JSON.stringify(expense))
+  },[expense])
+  // const firstRender = useRef(true)
+    
+    
+  // useEffect(()=>{
+  //     if(firstRender.current===true){
+  //       firstRender.current = false
+  //       return
+  //     }
+  //   localStorage.setItem("expenses",JSON.stringify(expense))
+  //   },[expense])
+  // setItem - This method used for saving items in local storage
+  // getItem - This method is used to take out data from the local storage
+  // useEffect(()=>{
+  //    const localData = JSON.parse(localStorage.getItem("expenses"))
+  //   setExpense(localData)
+
+  // },[])
   const[selectedCategory,setSelectedCategory]=useState("All")
   const[editingExpense,setEditingExpense]=useState(null)
   const[editingIndex,setEditingIndex]=useState(null)
+  const[searchTerm,setSearchTerm]= useState("")
+  const[sortOrder,setSortOrder] = useState("Low → High")
   console.log(expense)
+  
   function handleAddExpense(newExpense){
     const handling = [...expense,newExpense] 
     setExpense(handling)
      }
   function handleDeleteExpense(deleteIndex){
+    const confirmDelete = confirm("Are you sure you want to delete this expense?")
+    // confirm- return value true if ok,false in cancel
+    if(confirmDelete===false){
+      return
+    }
     const updatedExpense=expense.filter((item,index)=>{
       return index!==deleteIndex
      
@@ -20,13 +50,27 @@ const App = () => {
   }
   function handleEditExpense(editIndex){
     const currentExpense = expense[editIndex]
-    console.log("EDIT CLICK", currentExpense)
+    // console.log("EDIT CLICK", currentExpense)
     setEditingExpense(currentExpense)
     setEditingIndex(editIndex)
   }
   function handleUpdateExpense(updatedExpense){
+    const changedExpense = expense.map((item,index)=>{
+      if(index === editingIndex){
+        return updatedExpense
+      }
+      return item
+
+    })
+    setExpense(changedExpense)
+    setEditingExpense(null)
 
   }
+  function handleCancelEdit(){
+    setEditingExpense(null)
+    setEditingIndex(null)
+  }
+  
   
   const total = expense.reduce((total,item)=>{
     return total + Number(item.amount)
@@ -38,22 +82,46 @@ const App = () => {
 // const filteredExpense= expense.filter((item,index)=>{
 //   return item.category===new_category
 // })}
-  const filteredExpense = expense.filter((item)=>{
-   return item.category === selectedCategory || selectedCategory === "All"
+  const filteredExpense = expense
+  .map((item,index)=>{
+    return{
+      item:item,
+      index:index
+  }
+
   })
+  .filter((item,index)=>{
+   return (item.item.category === selectedCategory || selectedCategory === "All")
+    &&
+    (item.item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  })
+  .sort((a,b)=>{
+    if(sortOrder==="Low → High"){
+    return Number(a.item.amount) - Number(b.item.amount)
+    }
+    return Number(b.item.amount) - Number(a.item.amount)
+  })
+  // Sorting: expenses ko amount ke basis par Low → High ya High → Low arrange karna.
+// sort() comparator rule: Negative → a first, Positive → b first, Zero → equal.
+// Low → High: a.amount - b.amount
+// High → Low: b.amount - a.amount
+  const filteredTotal = filteredExpense.reduce((total,item)=>{
+    return total + Number(item.item.amount)
+  },0)
   return (
-    <div><h1>Expense Tracker 💸</h1>
+    <div className="app"><h1>Expense Tracker 💸</h1>
       <Add onAdd={handleAddExpense}
         editingExpense={editingExpense}
+        onUpdate={handleUpdateExpense}
+        onCancelEdit={handleCancelEdit}
       />
-      {
-        filteredExpense.map((item,index)=>{
-          return(
-        <p key={index}>{item.name}-{item.amount}-{item.category}
-        &nbsp;<button onClick={()=>handleEditExpense(index)}>Edit</button>
-        &nbsp;<button onClick={()=>handleDeleteExpense(index)}>Delete</button></p>)
-})}
-<h2>Total Expense:{total}</h2>
+      
+ <div className="factors">
+ <div className="filter">
+ <label>Search :</label>
+ <input  onChange = {(e)=>{setSearchTerm(e.target.value)}}/></div>
+ <div className="filter">
+  <label>Category :</label>
 <select onChange={(e)=>setSelectedCategory(e.target.value)}>
   <option>All</option>
   <option>Food</option>
@@ -61,6 +129,49 @@ const App = () => {
   <option>Shopping</option>
   <option>Bills</option>
 </select>
+</div>
+<div className="filter">
+<label>Sort :</label>
+<select onChange={(e)=>{setSortOrder(e.target.value)}}>
+    <option>Low → High</option>
+    <option>High → Low</option>
+  </select>
+  </div>
+    </div>
+     
+      <div className="expense-list">
+        <h3>Expense List</h3>
+        <div className="expense-header">
+          <div className="expense-header_details">
+          <span>Name</span>
+          <span>Amount</span>
+          <span>Category</span>
+          </div>
+          <div className="expense-header_action">
+          <span>Action</span>
+          </div>
+        </div>
+      {
+        filteredExpense.map((item,index)=>{
+          return(
+             
+        <div className ="expense-item" key={index}>
+        
+       <div className="expense-info">
+      
+        <span>{item.item.name}</span>
+        <span>{item.item.amount}</span>
+        <span>{item.item.category}</span>
+       </div>
+       <div className="expense-buttons">
+        &nbsp;<button onClick={()=>handleEditExpense(item.index)}>Edit</button>
+        &nbsp;<button onClick={()=>handleDeleteExpense(item.index)}>Delete</button></div></div>)
+    
+})}
+</div>
+{/* <h2>Total Expense:{total}</h2>
+(it shows-sum of all expenses and filteredExpenses shows only of filtered items only ) */}
+  <h2>Total Expense : {filteredTotal}</h2> 
     </div>
   )
 }
--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -1,9 +1,10 @@
 import React,{useState ,useEffect} from 'react'
 
-const AddExpense = ({onAdd , editingExpense}) => {
+const AddExpense = ({onAdd , editingExpense ,onUpdate ,onCancelEdit }) => {
     const [name,setName] = useState("")
     const [amount,setAmount] = useState("")
     const [category,setCategory] = useState("Food")
+    const[error,setError] = useState("")
     useEffect(()=>{
       if(editingExpense){
         setName(editingExpense.name)
@@ -12,27 +13,77 @@ const AddExpense = ({onAdd , editingExpense}) => {
 
       }
 
-    })
+    },[editingExpense])
     function addExpense(){
+      if(name.trim() === ""){
+          setError("Name field is empty.")
+        return 
+      }
+      if( !/^[A-Za-z0-9 ]+$/.test(name)){
+        setError("Expense name contains invalid characters.")
+        return
+      }
+      if(! /[A-Za-z]/.test(name)){
+          console.log("LETTER CHECK FAILED")
+        setError("Atleast one letter required.")
+        return
+      }
+      if( amount === ""){
+          setError("Amount field is empty.")
+        return 
+      }
+      if(isNaN(amount)===true){
+        setError("Amount is invalid.")
+        return
+      }
+      if(amount <= 0){
+        setError("Amount must greater than 0.")
+        return
+      }
+      
+      setError("")
         const expenseData={ 
+          id: Date.now(),
+          // Date.now=>se time aara hai miliseconds mai mere bhai
           name,
           amount,
           category
         }
+        if(editingExpense){
+          onUpdate(expenseData)
+         
+          setName("")
+          setAmount("")
+          setCategory("Food")
+
+        }
+        else{
         onAdd(expenseData)
         // console.log(name,amount,category)
         setName("")
         setAmount("")
         setCategory("Food")
-    }
+    }}
   return (
-    <div><h2>AddExpense</h2>
-    <label>Name:</label>
-    <input placeholder="Enter Name" value={name} onChange={(e)=>setName(e.target.value)}/><br/>
-    
-     <label>Amount:</label>
+    <div className = "form-content">
+    <div className="expense-form"><h2>AddExpense</h2>
+    {error !== "" && <p style = {{color:"red",fontSize:"13px"}}>{error}</p>}
+    <div className="expense-name">
+    <label>Name : </label>
+    <input placeholder="Enter Name" value={name} onChange={(e)=>{
+      setName(e.target.value)
+      setError("")
+      }}/><br/>
+      </div>
+      <div>
+     <label>Amount : </label>
 
-     <input  placeholder="Enter Amount"  value={amount} onChange={(e)=>setAmount(e.target.value)}/><br/>
+     <input  placeholder="Enter Amount"  value={amount} onChange={(e)=>{
+      setAmount(e.target.value)
+      setError("") 
+      }}/><br/>
+      </div>
+      <div className="expense-category">
      <label>Category:</label>
      <select value={category} onChange={(e)=>setCategory(e.target.value)}><option>Food</option>
 
@@ -40,11 +91,17 @@ const AddExpense = ({onAdd , editingExpense}) => {
      <option>Shopping</option>
      <option>Bills</option></select><br/>
                {/* <p>{category}</p> */}
+                    </div>
      <button onClick={addExpense}>{editingExpense ? "Update" : "Add"}</button>
-    
-    </div>
-  )
-  
-}
-
+</div>
+    &nbsp;{ editingExpense && (
+      <button onClick={()=>{
+        onCancelEdit()
+    setName("")
+    setAmount("")
+    setCategory("Food")
+    }}>Cancel</button>
+  )}
+</div>
+)}
 export default AddExpense
